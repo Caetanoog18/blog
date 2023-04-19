fc0a63cfb71cb7ed5a8739586b69c2a2b18334c3
Gabriel Caetano <caetanoog@outlook.com>
2023-04-19 19:58:49 +0200

    adding search tab and improving blog page

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
+from django.db.models import Q, Count, Case, When
 
 class PostIndex(ListView):
     model = Post
@@ -9,11 +10,53 @@ class PostIndex(ListView):
     paginate_by = 5
     context_object_name = 'posts'
 
-class PostSearch(PostIndex):
-    pass
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.select_related('category_post')
+        qs = qs.order_by('-id').filter(published_post = True)
+        qs = qs.annotate(
+            number_comments = Count(
+                Case(
+                    When(comment__published_comment = True, then=1)
+                )
+            )
+        )
 
+        return qs
+
+
+class PostSearch(PostIndex):
+    template_name = 'posts/post_search.html'
+    def get_queryset(self):
+        qs = super().get_queryset()
+        term = self.request.GET.get('term')
+
+        if not term:
+            return qs
+
+        qs = qs.filter(
+            Q(title_post__icontains=term) |
+            Q(author_post__first_name__iexact=term) |
+            Q(content_post__icontains=term) |
+            Q(excerpt_post__icontains=term) |
+            Q(category_post__name_cate__iexact=term)
+        )
+
+        return qs
+    
 class PostCategory(PostIndex):
-    pass
+    template_name = 'posts/post_category.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category = self.kwargs.get('category', None)
+
+        if not category:
+            return qs
+
+        qs = qs.filter(category_post__name_cate__iexact=category)
+        return qs
 
 class PostDetails(UpdateView):
     pass
